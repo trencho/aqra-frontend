1ad55d3279bb9ecf1ad1637518abf398e93063cf
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-02-20 11:43:25 +0100

    Updated dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,26 @@ import pluginVue from "eslint-plugin-vue";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,vue}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parserVue,
+      parserOptions: {
+        parser: parserBabel,
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/no-reserved-component-names": "off",
+      "vue/require-default-prop": "off",
+    },
+  },
 ];
